c7321bb3f526f7a2e5eb81bc8d49300e963be7ec
Abe Massry <abe@massindustries.org>
2012-05-21 22:49:33 -0400

    temporarily changed vars to work on site will change back when project is further along

--- class/vars.php
+++ class/vars.php
@@ -1,25 +1,29 @@
 <?php
 
+//commented while working on this code, will replace when project is further 
+//along
+//replaced with this line
+require_once '../../vars.php';
 
 //SQL vars
-$sql_host = 'localhost';
-$sql_user = 'gravity-user';
-$sql_pass = 'gravity-pass';
-$sql_db = 'gravity';
+//$sql_host = 'localhost';
+//$sql_user = 'gravity-user';
+//$sql_pass = 'gravity-pass';
+//$sql_db = 'gravity';
 
 //directory vars
-$php_dir = '';
-$html_dir = '';
-
-function getPHPDir() {
-    $php_dir = '/var/www/gravity';
-    return $php_dir;
-}
-
-function getHTMLDir() {
-    $html_dir = '/gravity';
-    return $html_dir;
-}
+//$php_dir = '';
+//$html_dir = '';
+//
+//function getPHPDir() {
+//    $php_dir = '/var/www/gravity';
+//    return $php_dir;
+//}
+//
+//function getHTMLDir() {
+//    $html_dir = '/gravity';
+//    return $html_dir;
+//}
 
 function getPage() {
     if ($_GET['page']) {
